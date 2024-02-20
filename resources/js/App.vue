853e36892bd5bd172c510bb0011f40e8cac48cf3
annon-prog <fidelisdela.wambui@gmail.com>
2024-02-20 12:47:48 +0300

    project setup

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+
+<template>
+    <h1>Vue template guide</h1>
+    <p>This is a simple test for integrating Vue into a Laravel project. Vue should act as the frontend integration medium for Laravel while Laravel deals with the backend API endpoints.</p>
+</template>
+<script>
+export default {
+    setup() {
+        
+    }
+}
+</script>
+
